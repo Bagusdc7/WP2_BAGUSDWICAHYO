058aa38684e452312ccd3346903282beb394cb94
Bagusdc7 <bagusdwicahyo77@gmail.com>
2022-03-24 16:30:41 +0700

    menambahkanbiodata.php git commit -m menambahkan

---
+++ biodata.php
@@ -0,0 +1,21 @@
+<?php
+class Biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN </h1>";
+        echo " <p> BIODATA DIRI SAYA</p>";
+        echo " <p> NAMA                : BAGUS DWI CAHYO </p>";
+        echo " NIM                     : 17190079 ";
+        echo " <p> KELAS               : 17.6A.25 </p>";
+        echo " Fakultas                : Teknologi INformasi";
+        echo " <p> UNIVERSITAS         : BINA SARANA INFORMATIKA</p>";
+        echo " JENIS KELAMIN           : LAKI-LAKI";
+        echo " <p>TEMPAT,TANGGAL LAHIR : JAKARTA, 25 AGUSTUS 1996</p>";
+        echo " GOLONGAN DARAH          : A";
+        echo " <p>ALAMAT               : JL VETERAN 1 B KALIDERES</p>";
+        echo " HOBBY                   : TIDUR ";
+        echo " <p> E-MAIL              : Bagusdwicahyo77@gmail.com</p>";
+        echo " NO.HP                   : 081286162151";
+    }
+)
